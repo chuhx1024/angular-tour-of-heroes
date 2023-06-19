b5c27c417cffee8c49759cb3f775056792e60d0d
chuhx <chuhx1024@126.com>
2023-06-19 19:15:53 +0800

    feat: set @ 别名 alias

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NgFormComponent } from "@/app/view/ng-form/ng-form.component";
+// import { NgSwitchComponent } from "./view/ng-switch/ng-switch.component";
+import { NgSwitchComponent } from "@/app/view/ng-switch/ng-switch.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'ng-form', component: NgFormComponent},
+  {path: 'ng-switch', component: NgSwitchComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
