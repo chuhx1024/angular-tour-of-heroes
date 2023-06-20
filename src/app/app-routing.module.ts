05422d9fbf723a67b61bd0e3c463637c9e0affa8
chuhx <chuhx1024@126.com>
2023-06-20 11:27:40 +0800

    feat: 路由404匹配

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NgFormComponent } from "@/app/view/ng-form/ng-form.component";
-// import { NgSwitchComponent } from "./view/ng-switch/ng-switch.component";
 import { NgSwitchComponent } from "@/app/view/ng-switch/ng-switch.component";
 
 const routes: Routes = [
   {path: 'ng-form', component: NgFormComponent},
-  {path: 'ng-switch', component: NgSwitchComponent}
+  {path: 'ng-switch', component: NgSwitchComponent},
+  {path: '**', component: NgFormComponent},
 ];
 
 @NgModule({
